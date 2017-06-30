2ed8ebd55f0b9523ab69b27857a6cd63d5881900
Magnus Gustafsson <magnus.gustafsson@king.com>
2017-06-30 19:36:38 +0100

    Implemented basic controls

---
+++ javafx-renderer/src/main/java/se/fearless/salix/MouseHandler.java
@@ -0,0 +1,97 @@
+package se.fearless.salix;
+
+import javafx.event.EventHandler;
+import javafx.scene.PerspectiveCamera;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.ScrollEvent;
+
+public class MouseHandler {
+    private static final double CONTROL_MULTIPLIER = 0.1;
+    private static final double SHIFT_MULTIPLIER = 10.0;
+    private static final double MOUSE_SPEED = 0.1;
+    private static final double ROTATION_SPEED = 2.0;
+    private static final double TRACK_SPEED = 0.3;
+
+
+    private double mousePosX;
+    private double mousePosY;
+    private double mouseOldX;
+    private double mouseOldY;
+    private double mouseDeltaX;
+    private double mouseDeltaY;
+
+
+    final PerspectiveCamera camera;
+    final Xform cameraXform;
+    final Xform cameraXform2;
+    private double modifierFactor = 0.1;
+
+
+    public MouseHandler(PerspectiveCamera camera, Xform cameraXform, Xform cameraXform2) {
+        this.camera = camera;
+        this.cameraXform = cameraXform;
+        this.cameraXform2 = cameraXform2;
+    }
+
+    public void handleMouse(Scene scene) {
+
+
+        scene.setOnScroll(new EventHandler<ScrollEvent>() {
+            @Override
+            public void handle(ScrollEvent event) {
+                event.getDeltaX();
+                double z = camera.getTranslateZ();
+                double newZ = z + event.getDeltaY() * MOUSE_SPEED * 10;
+                camera.setTranslateZ(newZ);
+            }
+        });
+
+        scene.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent me) {
+                mousePosX = me.getSceneX();
+                mousePosY = me.getSceneY();
+                mouseOldX = me.getSceneX();
+                mouseOldY = me.getSceneY();
+            }
+        });
+        scene.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent me) {
+                mouseOldX = mousePosX;
+                mouseOldY = mousePosY;
+                mousePosX = me.getSceneX();
+                mousePosY = me.getSceneY();
+                mouseDeltaX = (mousePosX - mouseOldX);
+                mouseDeltaY = (mousePosY - mouseOldY);
+
+                double modifier = 1.0;
+
+                if (me.isControlDown()) {
+                    modifier = CONTROL_MULTIPLIER;
+                }
+                if (me.isShiftDown()) {
+                    modifier = SHIFT_MULTIPLIER;
+                }
+                if (me.isSecondaryButtonDown()) {
+                    cameraXform.ry.setAngle(cameraXform.ry.getAngle() -
+                            mouseDeltaX * modifierFactor * modifier * ROTATION_SPEED);  //
+                    cameraXform.rx.setAngle(cameraXform.rx.getAngle() +
+                            mouseDeltaY * modifierFactor * modifier * ROTATION_SPEED);  // -
+                } else if (me.isPrimaryButtonDown()) {
+                    double z = camera.getTranslateZ();
+                    double newZ = z + mouseDeltaX * MOUSE_SPEED * modifier;
+                    camera.setTranslateZ(newZ);
+                } else if (me.isMiddleButtonDown()) {
+                    cameraXform2.t.setX(cameraXform2.t.getX() +
+                            mouseDeltaX * MOUSE_SPEED * modifier * TRACK_SPEED);  // -
+                    cameraXform2.t.setY(cameraXform2.t.getY() +
+                            mouseDeltaY * MOUSE_SPEED * modifier * TRACK_SPEED);  // -
+                }
+
+            }
+        });
+    }
+}
--- javafx-renderer/src/main/java/se/fearless/salix/ViewApplication.java
+++ javafx-renderer/src/main/java/se/fearless/salix/ViewApplication.java
@@ -1,25 +1,102 @@
 package se.fearless.salix;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Box;
 import javafx.stage.Stage;
 
 public class ViewApplication extends Application {
-    private final BorderPane pane=new BorderPane();
+    private static final double AXIS_LENGTH = 250.0;
 
+    private final BorderPane pane = new BorderPane();
+    final Group root = new Group();
+    final Xform world = new Xform();
+
+    final PerspectiveCamera camera = new PerspectiveCamera(true);
+    final Xform cameraXform = new Xform();
+    final Xform cameraXform2 = new Xform();
+    final Xform cameraXform3 = new Xform();
+    private static final double CAMERA_INITIAL_DISTANCE = -450;
+    private static final double CAMERA_INITIAL_X_ANGLE = 70.0;
+    private static final double CAMERA_INITIAL_Y_ANGLE = 320.0;
+    private static final double CAMERA_NEAR_CLIP = 0.1;
+    private static final double CAMERA_FAR_CLIP = 10000.0;
+
+    final Xform axisGroup = new Xform();
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 
     @Override
     public void start(Stage stage) throws Exception {
-        SalixMouseEventHandler salixMouseEventHandler = new SalixMouseEventHandler();
-        final Scene scene = new Scene(pane, 880, 680, true);
-        scene.addEventHandler(MouseEvent.ANY, salixMouseEventHandler);
+        // SalixMouseEventHandler salixMouseEventHandler = new SalixMouseEventHandler();
+
+
+        Scene scene = new Scene(root, 1024, 768, true);
+        scene.setFill(Color.GREY);
+
+//        scene.addEventHandler(MouseEvent.ANY, salixMouseEventHandler);
+
+        //pane.setCenter(salixRenderer.getNode());
+
         //scene.cursorProperty().bind(rubik.getCursor());
-        scene.setFill(Color.ALICEBLUE);
         stage.setTitle("Salix View");
         stage.setScene(scene);
+
+        buildCamera();
+        buildAxes();
+
+
+        scene.setCamera(camera);
+        root.getChildren().addAll(world);
+        MouseHandler mouseHandler = new MouseHandler(camera, cameraXform, cameraXform2);
+        mouseHandler.handleMouse(scene);
         stage.show();
+    }
+
+    private void buildCamera() {
+        root.getChildren().add(cameraXform);
+        cameraXform.getChildren().add(cameraXform2);
+        cameraXform2.getChildren().add(cameraXform3);
+        cameraXform3.getChildren().add(camera);
+        cameraXform3.setRotateZ(180.0);
+
+        camera.setNearClip(CAMERA_NEAR_CLIP);
+        camera.setFarClip(CAMERA_FAR_CLIP);
+        camera.setTranslateZ(CAMERA_INITIAL_DISTANCE);
+        cameraXform.ry.setAngle(CAMERA_INITIAL_Y_ANGLE);
+        cameraXform.rx.setAngle(CAMERA_INITIAL_X_ANGLE);
+    }
+
+    private void buildAxes() {
+        final PhongMaterial redMaterial = new PhongMaterial();
+        redMaterial.setDiffuseColor(Color.DARKRED);
+        redMaterial.setSpecularColor(Color.RED);
+
+        final PhongMaterial greenMaterial = new PhongMaterial();
+        greenMaterial.setDiffuseColor(Color.DARKGREEN);
+        greenMaterial.setSpecularColor(Color.GREEN);
+
+        final PhongMaterial blueMaterial = new PhongMaterial();
+        blueMaterial.setDiffuseColor(Color.DARKBLUE);
+        blueMaterial.setSpecularColor(Color.BLUE);
+
+        final Box xAxis = new Box(AXIS_LENGTH, 1, 1);
+        final Box yAxis = new Box(1, AXIS_LENGTH, 1);
+        final Box zAxis = new Box(1, 1, AXIS_LENGTH);
+
+        xAxis.setMaterial(redMaterial);
+        yAxis.setMaterial(greenMaterial);
+        zAxis.setMaterial(blueMaterial);
+
+        axisGroup.getChildren().addAll(xAxis, yAxis, zAxis);
+        axisGroup.setVisible(true);
+        world.getChildren().addAll(axisGroup);
     }
 }
